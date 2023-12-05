cbfaacde3d484bbdc17476fdf4b3024f0f37458c
Danton Mariano <65569798+DantonMariano@users.noreply.github.com>
2023-12-05 10:23:39 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -173,7 +173,7 @@ def main(control = False) -> None:
         
         if answer == "Install New PHP Version":
             answer = input("Which version would you like to have installed? ")
-            available_options = ['5.6', '7.0', '7.1', '7.2','7.3', '7.4', '8.0', '8.1']
+            available_options = ['5.6', '7.0', '7.1', '7.2','7.3', '7.4', '8.0', '8.1', '8.2', '8.3']
             if answer not in available_options:
                 print(red("That's not an available version of PHP"))
                 main()
